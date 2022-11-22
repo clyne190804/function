7d0e536eaa445c472f0eb87eb314ab68fa480b68
clyne <clyne190804@gmail.com>
2022-11-22 23:44:22 +0900

    commit

--- common/CommonDate.java
+++ common/CommonDate.java
@@ -1,4 +1,50 @@
 package common;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class CommonDate {
+    /**
+     * 현재 일자를 리턴한다.
+     * ex)2022-11-20
+     * @return 현재 일자
+     */
+    public static String getNowDate(){
+        LocalDate localDate = LocalDate.now();
+        return localDate.toString().replaceAll("-", "");
+    }
+
+    /**
+     *
+     * 파라미터 형식에 맞는 현재 일자를 리턴한다.
+     * ex)2022-11-20 -> 20221120
+     * @param str 치환할 문자열
+     * @return 파라미터로 치환된 현재 일자
+     */
+    public String getNowDatePattern(String str){
+        LocalDate localDate = LocalDate.now();
+        return localDate.toString().replaceAll("-", str);
+    }
+
+    /**
+     * 현재 시간을 리턴한다.
+     * ex)20221120222632 -> 222632
+     * @return 현재 시간
+     */
+    public static String getNowTime(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")).substring(8);
+    }
+
+    /**
+     * 현재 일자와 시간을 리턴한다.
+     * ex)20221120222632
+     * @return 현재 일자와 시간
+     */
+    public static String getNowDateTime(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+    }
+
 }
--- common/CommonPath.java
+++ common/CommonPath.java
@@ -1,7 +1,84 @@
 package common;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
 public class CommonPath {
-    public static void main(String[] args) {
-        System.out.println("1312132123");
+
+    /**
+     *
+     *
+     * @return MAC, WINDOWS, LINUX, UNIX
+     */
+    public static String getOS(){
+        String osName = System.getProperty("os.name");
+        if(osName.contains("Mac")){
+            return "MAC";
+        }else if(osName.contains("Windows")){
+            return "WINDOWS";
+        }else if(osName.contains("Linux")){
+            return "LINUX";
+        }else if(osName.contains("Unix")){
+            return "UNIX";
+        }else{
+            return "not defined OS";
+        }
+
+    }
+
+    /**
+     *
+     * @return 만화디렉토리 경로
+     */
+    public static String getComicImgDir(){
+        return System.getProperty("user.dir")+"/out/artifacts/jsp_war_exploded/images";
+    }
+
+    /**
+     *
+     * @param path
+     * @return 파라미터 경로의 디렉터리 리스트
+     */
+    public List<String> getDirectoryList(String path){
+        List<String> directoryList = new ArrayList<>();
+        for (File info : Objects.requireNonNull(new File(path).listFiles())) {
+            if (info.isDirectory()) {
+                directoryList.add(info.getName());
+            }
+        }
+
+        List<String> directoryListSorted = directoryList.stream().sorted(Comparator.naturalOrder()).toList();
+        for(String str : directoryListSorted){
+            System.out.println("str"+str);
+        }
+
+        return directoryList;
+    }
+
+    /**
+     *
+     * @param path
+     * @param sortDivision
+     * @return
+     */
+    public List<String> getSortDirectoryList(String path, String sortDivision){
+
+        List<String> directoryList = new ArrayList<>();
+        for (File info : Objects.requireNonNull(new File(path).listFiles())) {
+            if (info.isDirectory()) {
+                directoryList.add(info.getName());
+            }
+        }
+
+        if(sortDivision.equals("ASC")){
+            return directoryList.stream().sorted(Comparator.naturalOrder()).toList();
+        }else if(sortDivision.equals("DESC")){
+            return directoryList.stream().sorted(Comparator.reverseOrder()).toList();
+        }else{
+            return directoryList.stream().sorted(Comparator.naturalOrder()).toList();
+        }
     }
 }
